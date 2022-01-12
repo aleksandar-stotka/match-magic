faca43ada4475ad91f646fc553b4feaf43073dcc
aleksandar angelevski <stotka123@yahoo.com>
2022-01-12 22:34:50 +0100

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+      real
+    </div>
+  );
+}
+
+export default App;
