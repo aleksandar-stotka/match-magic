f633fd766085c6cd4d264368797682214d751d42
aleksandar angelevski <stotka123@yahoo.com>
2022-01-13 00:20:22 +0100

    display-images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+/// shuffle cards
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages] //we want ti duplicate array
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
-      real
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img src={card.src} className="front" alt="card front" />
+              <img src="/img/cover.png" alt="cover" className="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
